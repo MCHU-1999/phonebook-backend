0a99b33e6fbdb14d545d00d01414858398200f46
mchu-1999 <jerry.ee06@nctu.edu.tw>
2024-05-27 19:22:44 +0800

    3.3: Phonebook backend step 3

--- index.js
+++ index.js
@@ -14,6 +14,20 @@ app.get('/api/persons', (request, response) => {
   response.status(200).json(data)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const found = data.find(element => element.id === id)
+
+  if (found) {
+    response.status(200).json(found)
+  } else {
+    response.status(404).json({
+      status: 'error',
+      message: `No data corresponds to id: ${id}`
+    })
+  }
+})
+
 app.get('/info', (request, response) => {
   response.send(`<p>Phonebook has info for ${data.length} people</p><p>${Date().toString()}</p>`)
 })
