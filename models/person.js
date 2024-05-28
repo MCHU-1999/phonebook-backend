c8e85c1c5f45fa4329d08a59fdf9aaa66cd24e1c
mchu-1999 <jerry.ee06@nctu.edu.tw>
2024-05-29 01:49:20 +0800

    3.20*: Phonebook database, step 8

--- models/person.js
+++ models/person.js
@@ -18,12 +18,18 @@ mongoose.connect(url)
     name: {
       type: String,
       minLength: 3,
-      required: true
+      required: [true, 'User name required']
     },
     number: {
       type: String,
-      minLength: 3,
-      required: true
+      validate: {
+        validator: function(v) {
+          return /^\d{2,3}-\d+$/.test(v);
+        },
+        message: props => `${props.value} is not a valid phone number!`
+      },
+      minLength: 8,
+      required: [true, 'User phone number required']
     },
   })
 
